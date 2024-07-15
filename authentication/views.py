00999e4bbf5d60b6c196c96e6145c3bded54e9c1
BillyWaks <136112711+BillyWaks@users.noreply.github.com>
2024-07-16 00:05:51 +0300

    teleafya_app

---
+++ authentication/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from authentication.serializers import RegisterSerializer
+from rest_framework import response
+
+# Create your views here.
+class RegisterAPIView(GenericAPIView):
+
+    serializer_class = RegisterSerializer
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return
