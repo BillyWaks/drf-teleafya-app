03565c424d5ac61b804e7c0b5da8d50512deb552
BillyWaks <136112711+BillyWaks@users.noreply.github.com>
2024-07-23 23:22:56 +0300

    add_cors_header

--- teleafya_api/settings.py
+++ teleafya_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'drf_yasg',
     'authentication',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -125,6 +127,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# allow anyone(domains) to connect to the server
+CORS_ALLOW_ALL_ORIGINS=True
 
 
 # Static files (CSS, JavaScript, Images)
