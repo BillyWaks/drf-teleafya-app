2715a6fc0ddbd200fe5273487070eb72fca9a375
BillyWaks <136112711+BillyWaks@users.noreply.github.com>
2024-07-26 13:14:05 +0300

    new_teleafya

--- teleafya_api/urls.py
+++ teleafya_api/urls.py
@@ -20,25 +20,30 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="teleafya API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-   authentication_classes=[]
+    openapi.Info(
+        title="TELEAFYA API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.ourapp.com/policies/terms/",
+        contact=openapi.Contact(email="contact@teleafya.local"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include("authentication.urls")),
-    path('api/teleafya/', include("teleafya.urls")),
+    path('auth/', include('authentication.urls')),
+    # path('social_auth/', include(('social_auth.urls', 'social_auth'),
+                                #  namespace="social_auth")),
+    # path('expenses/', include('expenses.urls')),
+    # path('income/', include('income.urls')),
+    path('', schema_view.with_ui('swagger',
+                                 cache_timeout=0), name='schema-swagger-ui'),
 
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('api/api.json/', schema_view.without_ui(cache_timeout=0),
+    #      name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+                                       cache_timeout=0), name='schema-redoc'),
 ]
